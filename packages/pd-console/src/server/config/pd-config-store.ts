fdbaad39a101411cc32984a84662b50f8dfbdeee
PD <csuzngjh@hotmail.com>
2026-09-15 01:19:01 +0800

    fix(pd-console,pd-cli): port to js-yaml 5 ESM-only imports (no default export)

--- packages/pd-console/src/server/config/pd-config-store.ts
+++ packages/pd-console/src/server/config/pd-config-store.ts
@@ -16,7 +16,7 @@
 
 import * as fs from 'fs';
 import * as path from 'path';
-import yaml from 'js-yaml';
+import * as yaml from 'js-yaml';
 import {
   validatePdConfig,
   computeEffectivePdConfig,
